b8a912758641d6f76c68e8a9a1cbf3453681251b
huangguangda <2397923107@qq.com>
2018-07-18 22:30:42 +0800

    1.0

--- app/src/main/java/cn/edu/gdmec/android/dashumobile/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/dashumobile/SplashActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
-
     private TextView mTvVersion;
     private String mVersion;
     @Override
